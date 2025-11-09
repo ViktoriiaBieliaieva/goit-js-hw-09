185562647233481194c17505bdc319ff9edb28bf
Viktoriia <viktoriiabieliaieva91@gmail.com>
2025-11-09 18:53:20 +0100

    add task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,37 @@
-console.log('Form');
+const form = document.querySelector('.feedback-form');
+const localStorageKey = "feedback-form-state";
+
+const formData = {
+    email: "",
+    message: "",
+}
+
+const savedData = JSON.parse(localStorage.getItem(localStorageKey));
+
+if (savedData) {
+  formData.email = savedData.email ?? "";
+  formData.message = savedData.message ?? "";
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener("input", (evt) => {
+    const { name, value } = evt.target;
+    formData[name] = value.trim();
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    if (formData.email === "" || formData.message === "") {
+        alert("«Fill please all fields»");
+        return;
+    }
+    
+    console.log(formData);
+    localStorage.removeItem(localStorageKey);
+    form.reset();
+
+    formData.email = "";
+    formData.message = "";
+});
